1b2a562e72b0dc5b38c744bf5dc452beeedc50f3
King <King@kakadeiMac.local>
2016-09-10 18:31:06 +0800

    回滚

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -17,9 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+<<<<<<< HEAD
     NSLog(@"测试");
     
     NSLog(@"测试哈哈哈啊");
+=======
+    
+>>>>>>> parent of e6700da... 测试1
 }
 
 - (void)didReceiveMemoryWarning {
